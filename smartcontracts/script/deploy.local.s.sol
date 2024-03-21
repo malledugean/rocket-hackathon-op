31f0969c97a296c0154ef6c2a21d3c22ddf4e8b8
Malledugean <pavusa@gmail.com>
2024-03-21 20:07:33 -0300

    added fixes from nrxschool / hackathon-op-template

--- smartcontracts/script/deploy.local.s.sol
+++ smartcontracts/script/deploy.local.s.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.8.18;
 import {Script, console2} from "forge-std/Script.sol";
 // import {Counter} from "../src/Counter.sol";
 
+import {Campaign} from "../src/Campaign.sol";
+import {Community} from "../src/Community.sol";
+import {CommunityManager} from "../src/CommunityManager.sol";
+
 contract Local is Script {
     // Counter counter;
+    Campaign campaign;
+    CommunityManager communityManager;
+    Community community;
+
+    address manager = 0x5fc7Bb8eBE8316B88390A10370fe1DA2FA481734;
+    address communityOwner1 = 0x22F7d0aa47F75D2D4Df9D6c8867117941Cbf4bf3;
 
     function setUp() public {}
 
@@ -14,6 +24,18 @@ contract Local is Script {
 
         // counter = new Counter();
         // console2.log("Counter address: ", address(counter));
+
+        communityManager = new CommunityManager(payable(manager));
+        console2.log("Counter address: ", address(communityManager));
+
+        community =
+            communityManager.addCommunity(1001, "comunidate Web3", "A melhor comunidade", communityOwner1);
+        console2.log("Counter address: ", address(community));
+
+        vm.prank(communityOwner1);
+        campaign = community.addCampaign{value: 0.00005 ether}("Super campanha 2024 para aprendizado em Web3", 5 ether);
+        console2.log("Counter address: ", address(campaign));
+        vm.stopPrank();
 
         vm.stopBroadcast();
     }
